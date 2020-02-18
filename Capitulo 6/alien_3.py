53678f26dfc1bc54e37c5c4d0461497ccf6c436f
edivaldo.junior <er.moura88@yahoo.com.br>
2020-02-18 14:56:49 -0300

    Exercícios resolvidos e exemplos dos capítulos do livro: CURSO INTENSIVO DE PYTHON

---
+++ Capitulo 6/alien_3.py
@@ -0,0 +1,18 @@
+"""
+Vamos adicionar duas novas informações ao dicionário alien_0: as
+coordenadas x e y do alienígena, que nos ajudarão a exibir o alienígena em
+determinada posição da tela. Vamos colocar o alienígena na borda
+esquerda da tela, 25 pixels abaixo da margem superior.
+
+Como as coordenadas da tela normalmente começam no canto superior esquerdo da
+tela, posicionaremos o alienígena na margem esquerda definindo a
+coordenada x com 0, e 25 pixels abaixo da margem superior, definindo a
+coordenada y com o valor 25 positivo, como vemos aqui
+"""
+
+alien_0 = {'cor': 'verde', 'pontuação': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
---
+++ Capitulo 6/alien_4.py
@@ -0,0 +1,14 @@
+"""
+Às vezes, é conveniente ou até mesmo necessário começar com um
+dicionário vazio e então acrescentar novos itens a ele. Para começar a
+preencher um dicionário vazio, defina-o com um conjunto de chaves vazio
+e depois acrescente cada par chave-valor em sua própria linha. Por
+exemplo, eis o modo de criar o dicionário alien_0 usando esta abordagem
+"""
+
+alien_0 = {}
+
+alien_0['color'] = 'verde'
+alien_0['pontuação'] = 5
+
+print(alien_0)
