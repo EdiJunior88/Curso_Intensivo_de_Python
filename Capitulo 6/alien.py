cf8df86ad2472ea410f1d4675802859008c0d859
edivaldo.junior <er.moura88@yahoo.com.br>
2020-02-18 14:36:08 -0300

    Exercícios resolvidos e exemplos dos capítulos do livro: CURSO INTENSIVO DE PYTHON

---
+++ Capitulo 6/alien.py
@@ -0,0 +1,10 @@
+"""
+Considere um jogo com alienígenas que possam ter cores e valores de
+pontuação diferentes. O dicionário simples a seguir armazena informações
+sobre um alienígena em particular
+"""
+
+alien_0 = {'cor': 'verde', 'pontos': 5}
+
+print(alien_0['cor'])
+print(alien_0['pontos'])
